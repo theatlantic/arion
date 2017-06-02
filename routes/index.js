30336b7845ddbf62d06af6fc4e1074d3291f8531
Jeremy Green <mutatedcreativity@gmail.com>
2017-06-02 10:52:02 -0400

    upate thing

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,16 @@ const router = express.Router();
 
 router.post('/pull-requests', (req, res) => {
   const body = req.body;
-  console.log(body);
+  console.log(typeof body);
 
   const action = body.action;
   console.log(action);
-  //if (action !== 'review_requested') {
-  //
-  //}
+  if (action !== 'review_requested') {
+    res.status(400).send('Bad Request');
+    return;
+  }
+
+
 });
 
 router.get('*', (req, res) => {
