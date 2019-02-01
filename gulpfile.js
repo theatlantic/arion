75f54c2958ddf15dab8a003af34a8f2046521466
Joey Nichols <joeyquarters@gmail.com>
2019-01-31 22:31:49 -0800

    Remove gulp-livereload dependency

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,15 @@
-var gulp = require('gulp'),
-  nodemon = require('gulp-nodemon'),
-  livereload = require('gulp-livereload');
+var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
 
 gulp.task('develop', function () {
-  livereload.listen();
   nodemon({
     script: 'bin/www',
     ext: 'js handlebars coffee',
     stdout: false
   }).on('readable', function () {
-    this.stdout.on('data', function (chunk) {
-      if(/^Express server listening on port/.test(chunk)){
-        livereload.changed(__dirname);
-      }
-    });
     this.stdout.pipe(process.stdout);
     this.stderr.pipe(process.stderr);
   });
 });
 
-gulp.task('default', [
-  'develop'
-]);
+gulp.task('default', ['develop']);
